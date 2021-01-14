c1dfee9e13e9ff413a1c59e799bcdafcec114948
Midhet Sulemani <midhetfatema94@gmail.com>
2021-01-14 21:18:27 +0300

    completed basic functionality with accessibility for color

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,11 +6,61 @@
 //
 
 import SwiftUI
+import CoreHaptics
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
+    }
+}
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                if differentiateWithoutColor {
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+                
+                ZStack {
+                    ForEach(0 ..< cards.count, id: \.self) {index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                self.removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
